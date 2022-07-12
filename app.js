6d70f345925ed367527968ae05cebab0ffff3bf9
dethMastery <owner@detzz.in.th>
2022-07-12 21:28:16 +0700

    added: new proxy solution

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const proxy = require('express-http-proxy');
+const cors = require('cors');
 
 const wPort = 8090
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!')
-})
+app.use(cors());
+
+let cOPT = {
+    origin: '*',
+    optionsSuccessStatus: 200
+}
+
+app.use('/', cors(cOPT), proxy('http://nas.kizune.live:30'));
 
 app.listen(wPort, () => {
     console.log('Web start at port ' + wPort);
